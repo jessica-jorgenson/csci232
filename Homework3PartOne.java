410c53dcd652814515d5b7ab6ede51d1a6e000e0
Jessica Jorgenson <jessica.jorgenson@bsd7students.org>
2018-05-23 03:54:30 -0600

    Add files via upload

---
+++ Homework3PartOne.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework3;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jorge
+ */
+public class Homework3PartOne implements Runnable{
+    
+    int seconds;
+    int count;
+    String spacing = "";
+    String category;
+    
+    public Homework3PartOne(int sec){
+        seconds = sec;
+        category = Integer.toString(seconds);
+    }
+    
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                count++;
+                if("3".equals(category)){
+                    spacing = "    ";
+                }else{
+                    spacing = "";
+                }
+                Thread.sleep(seconds * 1000);
+                System.out.println(spacing + count);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Homework3PartOne.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Homework3PartOne first = new Homework3PartOne(1);
+        Homework3PartOne second = new Homework3PartOne(3);
+        Thread t1 = new Thread(first);
+        Thread t2 = new Thread(second);
+        
+        t1.start();
+        t2.start();
+    }
+}
---
+++ Homework3PartTwo.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework3;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jorge
+ */
+public class Homework3PartTwo implements Runnable {
+
+    static ArrayList<Integer> ints = new ArrayList<Integer>();
+    static int toMatch = 1;
+    int id;
+    int random;
+    Homework3PartTwo next;
+
+    public Homework3PartTwo(int theId) {
+        id = theId;
+    }
+
+    public void setNext(Homework3PartTwo nextGuy) {
+        next = nextGuy;
+    }
+
+    @Override
+    public void run() {
+        while (toMatch != id) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Homework3PartTwo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            random = (int) (Math.random() * 50 + 1);
+            ints.add(random);
+            System.out.println("Thread " + id + " inserted " + random);
+            toMatch = next.id;
+
+
+}
+
+public static void main(String[] args) throws InterruptedException {
+        Homework3PartTwo first = new Homework3PartTwo(1);
+        Homework3PartTwo second = new Homework3PartTwo(2);
+        Homework3PartTwo third = new Homework3PartTwo(3);
+        Homework3PartTwo fourth = new Homework3PartTwo(4);
+
+        first.setNext(second);
+        second.setNext(third);
+        third.setNext(fourth);
+        fourth.setNext(first);
+
+        Thread firstThread = new Thread(first);
+        Thread secondThread = new Thread(second);
+        Thread thirdThread = new Thread(third);
+        Thread fourthThread = new Thread(fourth);
+
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+        fourthThread.start();
+
+    }
+}
