b5f0a70e577a8c29d5f7b493bbe96d9fd3c9634e
Jessica Jorgenson <jessica.jorgenson@bsd7students.org>
2018-05-23 07:31:50 -0600

    added headers

--- Homework3PartThree.java
+++ Homework3PartThree.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Jessica Jorgenson
+ * CSCI 232
+ * This program provides the solution to part 3 
+ * of the programming section of the hoeework
  */
 package homework3;
 
